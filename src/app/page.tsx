72b8d405ad679ecd7d7c571e54ddc3578066ba3f
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-08-12 11:46:00 -0500

    project started

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+
+    </main>
+  );
+};
+
+export default Home;

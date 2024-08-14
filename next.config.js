d7ef2ed25172fba5b7a054e7a0006088c1198dde
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-08-13 23:12:20 -0500

    trying to solve image bug

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isProd ? '/fe-mentor-rock-paper-scissors' : '',
-  assetPrefix: isProd ? '/fe-mentor-rock-paper-scissors/' : '',
+  basePath: isProd ? "/fe-mentor-rock-paper-scissors" : "",
+  assetPrefix: isProd ? "/fe-mentor-rock-paper-scissors/" : "",
+  publicRuntimeConfig: {
+    basePath: isProd ? "/fe-mentor-rock-paper-scissors" : "",
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
-
     return config;
-  },
-  publicRuntimeConfig: {
-    basePath: isProd ? '/fe-mentor-rock-paper-scissors' : '',
   },
 };

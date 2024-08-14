9bd2132c48d01d776aff9ddb0fe8753d8b000f33
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-08-14 00:59:20 -0500

    trying svg as next Image component

--- src/app/components/scoreBoard/gameBoard/GameBoard.tsx
+++ src/app/components/scoreBoard/gameBoard/GameBoard.tsx
@@ -1,19 +1,19 @@
 import RockIcon from "@/../public/icon-rock.svg";
 import PaperIcon from "@/../public/icon-paper.svg";
 import ScissorsIcon from "@/../public/icon-scissors.svg";
-import TriangleIcon from "@/../public/bg-triangle.svg"; // Ensure this import is correct
 import styles from "../../../page.module.scss";
 import Icon from "../icon/Icon";
 import React from "react";
+import Image from "next/image";
 
 // prettier-ignore
 interface GameBoardProps { onChoice: (choice: string) => void; }
 
 const GameBoard: React.FC<GameBoardProps> = ({ onChoice }) => {
   const iconProps = {
-    paper: { src: PaperIcon, alt: "paper", buttonClass: styles.paper, value: "paper", onClick: onChoice },
-    scissors: { src: ScissorsIcon, alt: "scissors", buttonClass: styles.scissors, value: "scissors", onClick: onChoice },
-    rock: { src: RockIcon, alt: "rock", buttonClass: styles.rock, value: "rock", onClick: onChoice },
+    paper: { src: '/icon-paper.svg', alt: "paper", buttonClass: styles.paper, value: "paper", onClick: onChoice },
+    scissors: { src: '/icon-scissors.svg', alt: "scissors", buttonClass: styles.scissors, value: "scissors", onClick: onChoice },
+    rock: { src: '/icon-rock.svg', alt: "rock", buttonClass: styles.rock, value: "rock", onClick: onChoice },
   };
 
   return (
@@ -24,7 +24,8 @@ const GameBoard: React.FC<GameBoardProps> = ({ onChoice }) => {
       </div>
       <Icon {...iconProps.rock} />
       <div className={styles.svgContainer}>
-        <TriangleIcon aria-label="triangle" /> {/* Use TriangleIcon as a React component */}
+        {/* <TriangleIcon aria-label="triangle" />  */}
+        <Image unoptimized src={"/bg-triangle.svg"} width={500} height={500} alt="" />
       </div>
     </div>
   );
--- src/app/components/scoreBoard/icon/Icon.tsx
+++ src/app/components/scoreBoard/icon/Icon.tsx
@@ -1,16 +1,17 @@
 import styles from "../../../page.module.scss";
 import classNames from "classnames";
+import Image from "next/image";
 import React from "react";
 
 // prettier-ignore
-interface IconProps { src: React.FC<React.SVGProps<SVGSVGElement>>; alt: string; buttonClass?: string; iconClass?: string; value: string; onClick: (value: string) => void; }
+interface IconProps { src: string; alt: string; buttonClass?: string; iconClass?: string; value: string; onClick: (value: string) => void; }
 
-const Icon: React.FC<IconProps> = ({ src: SvgIcon, alt, buttonClass, iconClass, value, onClick }) => {
+const Icon: React.FC<IconProps> = ({ src, alt, buttonClass, iconClass, value, onClick }) => {
   return (
     <button className={classNames(styles.btn, buttonClass)} onClick={() => onClick(value)} value={value}>
       <div className={classNames(styles.iconWrapper, iconClass)}>
         <div className={styles.iconContainer}>
-          <SvgIcon aria-label={alt} />
+          <Image src={src} width={0} height={0} alt={alt} aria-label={alt} />
         </div>
       </div>
     </button>
